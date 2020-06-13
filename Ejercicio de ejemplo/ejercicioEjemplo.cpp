66102808872d482821f39458e0385c20a2832dfe
agustinbc <agustin.bernardo.sfe@gmail.com>
2020-06-13 12:23:14 -0300

    Mi primer commit!

---
+++ Ejercicio de ejemplo/ejercicioEjemplo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 30; i++)
+    {
+        cout<<"mi vieja mula ya no es lo que era"<<endl;
+    }
+    return 0;
+}
