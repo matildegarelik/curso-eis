2d02d5dca15dafa9b61cbdeb2b6397086936ad76
agustinbc <agustin.bernardo.sfe@gmail.com>
2020-07-08 14:46:19 -0300

    Clase 4

--- Clase 4/Ejemplos de clase/vector3d.cpp
+++ Clase 4/Ejemplos de clase/vector3d.cpp
@@ -2,77 +2,145 @@
 #include <cmath>
 
 using namespace std;
-
+template <class T>
 class vector3D
 {
-private:
-    float x, y, z;
+
+protected:
+    T x, y, z;
 
 public:
-    vector3D(float x_const, float y_const, float z_const)
+    vector3D()
     {
-        x = x_const;
-        y = y_const;
-        z = z_const;
+        x=0;
+        y=0;
+        z=0;
+    }
+    vector3D(T x_constr, T y_constr, T z_constr)
+    {
+        x = x_constr;
+        y = y_constr;
+        z = z_constr;
     }
 
-    vector3D operator+(vector3D other)
-    {
-        float x_res = x + other.x;
-        float y_res = y + other.y;
-        float z_res = z + other.z;
-
-        return vector3D(x_res, y_res, z_res);
-    }
-
-    vector3D operator-(vector3D other)
-    {
-        float x_res = x - other.x;
-        float y_res = y - other.y;
-        float z_res = z - other.z;
-
-        return vector3D(x_res, y_res, z_res);
-    }
-
-    vector3D operator*(float alpha)
-    {
-
-        float x_res = x * alpha;
-        float y_res = y * alpha;
-        float z_res = z * alpha;
-
-        return vector3D(x_res, y_res, z_res);
-    }
-
-    float modulo()
+    T calculaModulo()
     {
         return sqrt(x * x + y * y + z * z);
     }
 
-    float angulo()
+    vector3D operator+(vector3D other)
     {
-        return atan(y / x) * 180 / M_PI;
+        T x_res = x + other.x;
+        T y_res = y + other.y;
+        T z_res = z + other.z;
+
+        return vector3D(x_res, y_res, z_res);
     }
 
-    friend ostream& operator<<(ostream& os, const vector3D& vector)
+    vector3D operator*(T esc)
     {
-        os <<"(" <<vector.x<<","<<vector.y<<","<<vector.z<<")";
+        T x_res = x * esc;
+        T y_res = y * esc;
+        T z_res = z * esc;
+
+        return vector3D(x_res, y_res, z_res);
+    }
+
+    friend ostream &operator<<(ostream &os, const vector3D &vector)
+    {
+        os << "(" << vector.x << "," << vector.y << "," << vector.z << ")";
     }
 };
 
-class vector2D : public vector3D 
+template <class T>
+class vector2D : protected vector3D<T>
 {
-    public:
-        vector2D(float x_const, float y_const) : vector3D(x_const, y_const, 0) {};
+public:
+    vector2D(T x_constr, T y_constr) : vector3D<T>(x_constr, y_constr, 0) {};
+    
+    friend ostream &operator<<(ostream &os, const vector2D &vector)
+    {
+        os << "(" << vector.x << "," << vector.y << ")";
+    }
+    
+    vector2D operator+(vector2D other)
+    {
+        T x_res = this->x + other.x;
+        T y_res = this->y + other.y;
+
+        return vector2D(x_res, y_res);
+    }
+};
+
+template <class T_num>
+class sistema
+{
+private:
+    vector3D<T_num> fuerzas[50];
+    vector3D<T_num> posiciones[50]; // candado
+    int numeroActual;
+
+public:
+    sistema()
+    {
+        numeroActual = 0;
+    }
+    void agregarFuerza(vector3D<T_num> fuerzaEntrante, vector3D<T_num> puntoAplicacion)
+    {
+        fuerzas[numeroActual] = fuerzaEntrante;
+        posiciones[numeroActual] = puntoAplicacion;
+        numeroActual++;
+    }
+
+    void agregarFuerza(vector3D<T_num> fuerzaEntrante)
+    {
+        fuerzas[numeroActual] = fuerzaEntrante;
+        posiciones[numeroActual] = vector3D<T_num>(0., 0., 0.);
+        numeroActual++;
+    }
+
+
+    void quitarUltimaFuerza()
+    {
+        numeroActual--;
+    }
+
+    vector3D<T_num> fuerzaResultante()
+    {
+        vector3D<T_num> resultado;
+        for (int i = 0; i < numeroActual; i++)
+        {
+            resultado = resultado + fuerzas[i];
+        }
+        return resultado;
+    }
 };
 
 int main()
 {
-    vector2D w(3.0, 4.0);
+    /*
+    vector3D v(3., 4., 5.);
+    vector3D u(3., 5., 7.);
 
-    cout << w << endl;
+    vector3D w = u + v * 3;
+    vector3D t = w*2 + u*3;
 
-    cout << w.modulo() << " < " << w.angulo() << endl;
+    sistema S;
+
+    S.agregarFuerza(v);
+    S.agregarFuerza(u);
+    S.agregarFuerza(w);
+    S.agregarFuerza(t);
+
+    cout << S.fuerzaResultante() << endl;
+    */
+
+    vector2D<int> v(3, 4);
+    vector2D<int> u(1, 1);
+
+    vector2D<int> w = v+u;
+
+    cout<<w<<endl;
 
     return 0;
 }
---
+++ Clase 4/Ejemplos de clase/vectorFromStl.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    array<int, 3> a = {2, 3, 5};
+
+
+
+
+
+    cout<<a[2]<<endl;
+
+    return 0;
+}
